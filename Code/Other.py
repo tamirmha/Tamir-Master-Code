7312093d1fd788a218dafcbfcf818445c2859229
tamirmha <tamirmha@gmail.com>
2019-11-07 12:37:02 +0200

    add file that move the urdf of 6 dof to several folders

---
+++ Code/Other.py
@@ -0,0 +1,18 @@
+from os import environ, listdir, mkdir, path
+import shutil
+
+
+files_in_folder = 60000
+name = environ['HOME'] + "/urdf_6dof/"
+if not path.exists(name):
+    mkdir(name)
+
+full_path = environ['HOME'] + "/Tamir_Ws/src/manipulator_ros/Manipulator/man_gazebo/urdf/6dof/combined/"
+files = listdir(full_path)
+
+for j in range(len(files)/files_in_folder - 1):
+    if not path.exists(name+ str(j)):
+        mkdir(name + str(j))
+    for i in range(files_in_folder):
+        # shutil.copy(full_path + files[j*files_in_folder+i], name + str(j) + files[j*files_in_folder+i])
+        shutil.move(full_path + files[j*files_in_folder+i], name + str(j) + "/" + files[j*files_in_folder+i])
--- Code/simulator.py
+++ Code/simulator.py
@@ -323,8 +323,8 @@ if __name__ == '__main__':
         wait1_replace = 1.7
         wait2_replace = 1.2
     # default values
-    dofe = 4  # number degrees of freedom of the manipulator
-    link_max = 0.41  # max link length to check
+    dofe = 6  # number degrees of freedom of the manipulator
+    link_max = 0.71  # max link length to check
     start_arm = 0  # from which set of arms to start
     create_urdf = True
     # set parametrs from terminal
