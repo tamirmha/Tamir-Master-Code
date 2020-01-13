fix Z calculation during simulation add results

Code/Other.py
@@ -169,19 +169,19 @@ class FixFromJson(object):
                 jacobians, curr_poss = dat[row[2]]
                 mu = 1.1
                 z = 0
-                lci = 1.1
+                # lci = 1.1
                 for jacobian, curr_pos in zip(jacobians, curr_poss):
                     if jacobian != -1:
-                        mu_new, lci_new, z_new = self.indices_calc(row[2], jacobian, curr_pos)
+                        mu_new, z_new = self.indices_calc(row[2], jacobian, curr_pos)
                         if mu > mu_new:
                             mu = mu_new
                         if z < z_new:
                             z = z_new
-                        if lci > lci_new:
-                            lci = lci_new
+                        # if lci > lci_new:
+                        #     lci = lci_new
                 to_fix[to_fix.index(row)][7] = mu
                 to_fix[to_fix.index(row)][8] = z
-                to_fix[to_fix.index(row)][9] = lci
+                # to_fix[to_fix.index(row)][9] = lci
         return to_fix
 
     def indices_calc(self, names, jacobian, cur_pos):
@@ -191,23 +191,23 @@ class FixFromJson(object):
         # Manipulability index
         mu = round(np.product(j_ev), 3)  # self.manipulability_index(jacobian)
         # Local Conditioning Index
-        lci = round(j_ev[-1] / j_ev[0], 3)  # self.local_conditioning_index(jacobian)
+        # lci = round(j_ev[-1] / j_ev[0], 3)  # self.local_conditioning_index(jacobian)
         # Joint Mid-Range Proximity
         z = self.mid_joint_proximity(cur_pos, names)
-        return mu, lci, np.diag(z).max()
+        return mu, np.diag(z).max()
 
-    @staticmethod
-    def manipulability_index(jacobian):
-        n = jacobian.size / len(jacobian)
-        if n == 5:
-            det_j = np.linalg.det(np.matmul(np.transpose(jacobian), jacobian))
-        else:
-            det_j = np.linalg.det(np.matmul(jacobian, np.transpose(jacobian)))
-        if det_j > 1e-18:  # preventing numeric problems
-            return round(det_j ** (1/float(n)), 3)
-            # return round(det_j ** 0.5, 3)
-        else:
-            return 0
+    # @staticmethod
+    # def manipulability_index(jacobian):
+    #     n = jacobian.size / len(jacobian)
+    #     if n == 5:
+    #         det_j = np.linalg.det(np.matmul(np.transpose(jacobian), jacobian))
+    #     else:
+    #         det_j = np.linalg.det(np.matmul(jacobian, np.transpose(jacobian)))
+    #     if det_j > 1e-18:  # preventing numeric problems
+    #         return round(det_j ** (1/float(n)), 3)
+    #         # return round(det_j ** 0.5, 3)
+    #     else:
+    #         return 0
 
     @staticmethod
     def mid_joint_proximity(cur_pos, names):
@@ -749,7 +749,7 @@ def plot_data(result_file="/home/tamir/Tamir/Master/Code/results/recalculate/res
 
 
 # for optimization
-def assign_results(res_name="tosim/results_all_Success"):
+def assign_results(res_name="tosim/results_all_"):
     """ assign results from csv file """
     results = MyCsv.read_csv(res_name, "dict")
     x = []
@@ -955,12 +955,12 @@ if __name__ == '__main__':
     if to_merge:
         merge_data = MergeData()
         merge_data.merge()
+    if fix_all_from_json:
+        FixFromJson(all_files=True)
     if sumdata:
         summed_data = sum_data()
     if fix_from_json:
         FixFromJson()
-    if fix_all_from_json:
-        FixFromJson(all_files=True)
     if plotdata:
         plot_data(result_file="/home/tamir/Tamir/Master/Code/results/recalculate/results_all")
     if pareto_plot:

Code/simulator.py
@@ -202,7 +202,7 @@ class Simulator(object):
         jacobian = []
         curr_pos = []
         mu = []   # Manipulability index
-        lci = []  # Local Conditioning Index
+        # lci = []  # Local Conditioning Index
         z = []    # Joint Mid-Range Proximity
 
         arm_number = str(arm + 1 + k)
@@ -210,20 +210,20 @@ class Simulator(object):
             data_res.append(j[0])
             if j[0]:
                 mu.append(j[2][0])
-                lci.append(j[2][1])
-                z.append(j[2][2].min())
-                jacobian.append(j[2][3].tolist())
-                curr_pos.append(j[2][4].tolist())
+                # lci.append(j[2][1])
+                z.append(j[2][1].min())
+                jacobian.append(j[2][2].tolist())
+                curr_pos.append(j[2][3].tolist())
             else:
                 mu.append(-1)
-                lci.append(-1)
+                # lci.append(-1)
                 z.append(-1)
                 jacobian.append(-1)
                 curr_pos.append(-1)
         self.json_data.append({self.arms[arm]["name"]: [jacobian, curr_pos]})
         suc_res = "False"
         mu_min = -1
-        lci_min = -1
+        # lci_min = -1
         z_max = -1
         data_time = [-1, -1, -1, -1]
         avg_time = -1
@@ -234,7 +234,7 @@ class Simulator(object):
             data_time = [d[1] for d in data]
             avg_time = np.mean(data_time).round(2)
             mu = np.asarray(mu)
-            lci = np.asarray(lci)
+            # lci = np.asarray(lci)
             z = np.asarray(z)
             # choose only the min values because those are the "worst grade"
             try:
@@ -242,18 +242,18 @@ class Simulator(object):
             except:
                 self.save_json("mu_err", mu.tolist())
                 mu_min = -16
-            try:
-                lci_min = lci[lci >= 0.0].min()
-            except:
-                self.save_json("lci_err", lci.tolist())
-                lci_min = -16
+            # try:
+            #     lci_min = lci[lci >= 0.0].min()
+            # except:
+            #     self.save_json("lci_err", lci.tolist())
+            #     lci_min = -16
             try:
                 z_max = z[z > 0.0].max()
             except:
                 self.save_json("z_err", z.tolist())
                 z_max = -16
         return [arm_number, datetime.now().strftime("%d/%m/%y, %H:%M"), self.arms[arm]["name"], data_res,
-                str(data_time), suc_res,  str(avg_time), str(mu_min), str(z_max), str(lci_min)]
+                str(data_time), suc_res,  str(avg_time), str(mu_min), str(z_max)]
 
     def replace_model(self, arm):
         """
@@ -311,7 +311,7 @@ if __name__ == '__main__':
     elif username == "tamirm":  # VM
         nums = 25  # how many arms to send to simulator each time
         wait1_replace = 2.7
-        wait2_replace = 2.3
+        wait2_replace = 2.1
     else:
         nums = 25  # how many arms to send to simulator each time
         wait1_replace = 2.7